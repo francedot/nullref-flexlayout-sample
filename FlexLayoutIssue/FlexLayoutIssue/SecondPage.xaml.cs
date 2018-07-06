46714cdef15cf6ff3b576e794df972cdaa00cda5
Francesco Bonacci <francesco@Francescos-MBP.station>
2018-07-06 19:41:22 +0200

    Initial commit

---
+++ FlexLayoutIssue/FlexLayoutIssue/SecondPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FlexLayoutIssue
+{
+    public partial class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+
+            Device.StartTimer(TimeSpan.FromMilliseconds(250), () =>
+            {
+                FirstGrid.IsVisible = !FirstGrid.IsVisible;
+                return true;
+            });
+            Device.StartTimer(TimeSpan.FromMilliseconds(300), () =>
+            {
+                SecondGrid.IsVisible = !SecondGrid.IsVisible;
+                return true;
+            });
+            Device.StartTimer(TimeSpan.FromMilliseconds(350), () =>
+            {
+                ThirdGrid.IsVisible = !ThirdGrid.IsVisible;
+                return true;
+            });
+        }
+    }
+}
